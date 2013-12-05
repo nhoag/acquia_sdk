b405f494608cfbec185d9727ecd280de50567023
Jonathan Webb <jonathan.webb@acquia.com>
2013-12-05 15:20:28 -0500

    Adds unit test for Json.php

---
+++ test/Acquia/Common/JsonTest.php
@@ -0,0 +1,48 @@
+<?php
+
+class Acquia_Test_Common_Json extends PHPUnit_Framework_TestCase
+{
+    protected function getTestJson() {
+        return '{"foo":{"bar":"bar foo","Food":{"bar":"X","Fool":{"Foolery":{"ID":"LAMA","SortOf":"LAMA","Foot":"Lorem Aliquam Morbi Aenean","Aenean":"LAMA","Aliquam":"ABC 1234:5678","Foodie":{"para":"Lorem ipsum dolor sit amet, consectetur adipiscing elit.","FoosBall":["ABC","123"]},"Footsie":"quisquam"}}}}}';
+    }
+
+    protected function getTestArray() {
+        return array(
+            "foo" => array(
+                "bar" => "bar foo",
+                "Food" => array(
+                    "bar" => "X",
+                    "Fool" => array(
+                        "Foolery" => array(
+                            "ID" => "LAMA",
+                            "SortOf" => "LAMA",
+                            "Foot" => "Lorem Aliquam Morbi Aenean",
+                            "Aenean" => "LAMA",
+                            "Aliquam" => "ABC 1234:5678",
+                            "Foodie" => array(
+                                "para" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                                "FoosBall" => array(
+                                    "ABC",
+                                    "123"
+                                ),
+                            ),
+                            "Footsie" => "quisquam"
+                        )
+                    )
+                )
+            )
+        );
+    }
+
+
+    public function testJsonEncode()
+    {
+       $this->assertEquals(Acquia_Common_Json::encode($this->getTestArray()),$this->getTestJson());
+    }
+
+    public function testJsonDecode()
+    {
+        $this->assertEquals(Acquia_Common_Json::decode($this->getTestJson()),$this->getTestArray());
+    }
+}
+
