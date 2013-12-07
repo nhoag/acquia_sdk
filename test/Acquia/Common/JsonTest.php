da36dfe89b0590abf6b7d1136de58509551cb156
Jonathan Webb <webbj2012@jwwebb.info>
2013-12-07 17:37:59 -0500

    Debugging for PHP 5.2 JsonTest

--- test/Acquia/Common/JsonTest.php
+++ test/Acquia/Common/JsonTest.php
@@ -14,11 +14,22 @@ class Acquia_Test_Common_JsonTest extends PHPUnit_Framework_TestCase
 
     public function testJsonEncode()
     {
-       $this->assertEquals($this->getTestJson(), Acquia_Common_Json::encode($this->getTestArray()));
+        $expected = $this->getTestJson();
+        echo "\n\nEXPECTED:\n";
+        print_r($expected);
+
+        $actual = Acquia_Common_Json::encode($this->getTestArray());
+        echo "\n\nACTUAL:\n";
+        print_r($actual);
+        echo "\n\n";
+
+        $this->assertEquals($expected, $actual);
+
+        $this->assertEquals($this->getTestJson(), Acquia_Common_Json::encode($this->getTestArray()));
     }
 
     public function testJsonDecode()
     {
-        $this->assertEquals($this->getTestArray(), Acquia_Common_Json::decode($this->getTestJson()));
+         $this->assertEquals($this->getTestArray(), Acquia_Common_Json::decode($this->getTestJson()));
     }
 }
