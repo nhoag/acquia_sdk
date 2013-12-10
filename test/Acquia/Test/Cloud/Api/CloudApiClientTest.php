cb18c0992f6a09ad2592b212b2ebae77f7031d02
Jonathan Webb <jonathan.webb@acquia.com>
2013-12-10 16:29:02 -0500

    Adds mocking to CloudApiClientTest

--- test/Acquia/Test/Cloud/Api/CloudApiClientTest.php
+++ test/Acquia/Test/Cloud/Api/CloudApiClientTest.php
@@ -16,6 +16,33 @@ class Acquia_Test_Cloud_Api_CloudApiClientTest extends PHPUnit_Framework_TestCas
     }
 
     /**
+     * @param Acquia_Cloud_Api_CloudApiClient $cloudapi
+     * @param array $responseData
+     */
+    public function addMockResponse(Acquia_Cloud_Api_CloudApiClient $cloudapi, array $responseData)
+    {
+        $json = Acquia_Common_Json::encode($responseData);
+        $response = Acquia_Common_Json::decode($json);
+        $cloudapi->expects($this->any())
+            ->method('get')
+            ->will($this->returnValue($response));
+    }
+
+    /**
+     * @return Acquia_Cloud_Api_CloudApiClient
+     */
+    public function getMockCloudApiClient($methods = null)
+    {
+        return $this->getMock('Acquia_Cloud_Api_CloudApiClient', $methods, array(
+                'https://cloudapi.example.com',
+                array(
+                    'base_url' => 'https://cloudapi.example.com',
+                    'username' => 'test-username',
+                    'password' => 'test-password',
+                )));
+    }
+
+    /**
      * @expectedException RuntimeException
      */
     public function testRequireUsername()
@@ -51,6 +78,18 @@ class Acquia_Test_Cloud_Api_CloudApiClientTest extends PHPUnit_Framework_TestCas
     {
         $cloudapi = $this->getCloudApiClient();
         $this->assertEquals('/v1', $cloudapi->getConfig('base_path'));
+    }
+
+    public function testMockSitesCall()
+    {
+        $siteName = 'myhostingstage:mysitegroup';
+        $responseData = array($siteName);
+        $cloudapi = $this->getMockCloudApiClient(array('get'));
+        $this->addMockResponse($cloudapi, $responseData);
+
+        $sites = $cloudapi->sites();
+        $this->assertTrue($sites instanceof Acquia_Cloud_Api_Response_Sites);
+        $this->assertTrue($sites[$siteName] instanceof Acquia_Cloud_Api_Response_Site);
     }
 
 }
